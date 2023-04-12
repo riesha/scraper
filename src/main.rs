44d37f283b292724263d1273c1d7821abf7c76d7
riesha <seleneia@outlook.jp>
2023-04-12 14:37:27 +0200

    way better code and fixed scrape_all logic bug + proxy rotation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::net::SocketAddrV4;
+
 use anyhow::*;
-use reqwest::{Client, Url};
+use itertools::Itertools;
+use rand::seq::SliceRandom;
+use reqwest::{Client, Proxy, Url};
 use scraper::scrape;
+use tokio::fs;
 
 static URL: &str = "https://kaomoji-copy.com";
 
@@ -8,16 +13,35 @@ static URL: &str = "https://kaomoji-copy.com";
 async fn main() -> Result<()>
 {
     let url = Url::parse(URL)?;
-    let client = Client::builder().build()?;
+
+    let file = fs::read_to_string("proxies.txt").await?;
+
+    let proxies = file
+        .lines()
+        .filter_map(|line| {
+            line.parse::<SocketAddrV4>()
+                .ok()
+                .and_then(|addr| Url::parse(&addr.to_string()).ok())
+        })
+        .collect_vec();
+
+    let client = Client::builder()
+        .proxy(Proxy::custom(move |_url| {
+            proxies.choose(&mut rand::thread_rng()).cloned()
+        }))
+        .build()?;
 
     let mut visited_links = vec![];
     let mut links = vec![];
-    let _ = scrape::scrape_all(&client, url, &mut visited_links, &mut links).await?;
+    scrape::scrape_all(&client, url, &mut visited_links, &mut links).await?;
     dbg!(&links);
+    dbg!(&visited_links);
     println!(
         "Visited {} links for {} links scraped",
         visited_links.len(),
         links.len()
     );
+    // let images = scrape::get_all_images(&client, url).await?;
+    // dbg!(&images);
     Ok(())
 }
--- src/scrape.rs
+++ src/scrape.rs
@@ -1,19 +1,32 @@
 use anyhow::*;
 use async_recursion::async_recursion;
 use itertools::Itertools;
+
+use rand::seq::IteratorRandom;
 use reqwest::{Client, Url};
 use select::{document::Document, predicate::Name};
+use std::result::Result::Ok as StdOk;
+
+#[macro_export]
+macro_rules! bench {
+    ($func:stmt,$name:literal) => {
+        let start = Instant::now();
+        $func
+        let duration = start.elapsed();
+        println!("Time elapsed in {} is: {:?}", $name, duration);
+    };
+}
+
 pub async fn get_all_domestic_links(client: &Client, url: Url) -> Result<Vec<Url>>
 {
-    println!("Scraping [{}]...", url.as_str());
+    //println!("Scraping [{}]...", url.as_str());
     let content = client.get(url).send().await?.text().await?;
     let document = Document::from(content.as_str());
 
     let links: Vec<Url> = document
         .find(Name("a"))
-        .filter_map(|lnk| lnk.attr("href").map(|x| Url::parse(x.trim()).ok()))
+        .filter_map(|lnk| lnk.attr("href").and_then(|x| Url::parse(x.trim()).ok()))
         .unique()
-        .filter_map(|url| url)
         .collect();
 
     if links.is_empty()
@@ -25,54 +38,76 @@ pub async fn get_all_domestic_links(client: &Client, url: Url) -> Result<Vec<Url
         Ok(links)
     }
 }
+pub async fn get_all_images(client: &Client, url: Url) -> Result<Vec<Url>>
+{
+    let content = client.get(url).send().await?.text().await?;
+    let document = Document::from(content.as_str());
+
+    let images = document
+        .find(Name("img"))
+        .filter_map(|img| img.attr("src").and_then(|x| Url::parse(x.trim()).ok()))
+        .unique()
+        .collect_vec();
+    Ok(images)
+}
 #[async_recursion]
 pub async fn scrape_all(
     client: &Client, url: Url, visited: &mut Vec<Url>, links: &mut Vec<String>,
 ) -> Result<()>
 {
-    let std::result::Result::Ok(seed_links) = get_all_domestic_links(&client, url.clone()).await else {return Ok(())};
-    let og = Url::parse(&url.clone().origin().ascii_serialization())?;
-
-    let tasks: Vec<_> = seed_links
-        .into_iter()
-        .map(|lnk| {
-            let cl = client.clone();
-            let ur = if lnk.cannot_be_a_base()
-            {
-                og.join(lnk.as_str()).unwrap()
-            }
-            else
-            {
-                lnk
-            };
-            if !visited.contains(&ur)
-            {
-                println!("new link to visit! {}", ur.as_str());
-                visited.push(ur.clone());
-
-                Some(tokio::spawn(async move {
-                    get_all_domestic_links(&cl, ur).await
-                }))
-            }
-            else
-            {
-                None
-            }
-        })
-        .filter_map(|task| task)
-        .collect();
-
-    if tasks.is_empty()
+    if !visited.contains(&url)
     {
-        links.sort();
-        links.dedup();
-        return Ok(());
-    }
-    for task in tasks
-    {
-        let std::result::Result::Ok(task) = &mut task.await? else {continue};
-        links.append(&mut task.iter().map(|x| x.to_string()).collect_vec());
-    }
+        visited.push(url.clone());
+        println!("scrape_all with {}", url);
 
+        let StdOk(seed_links) = get_all_domestic_links(client, url.clone()).await else {return Ok(())};
+        let og = Url::parse(&url.clone().origin().ascii_serialization())?;
+
+        let tasks: Vec<_> = seed_links
+            .into_iter()
+            .filter_map(|lnk| {
+                let cl = client.clone();
+                let ur = if lnk.cannot_be_a_base()
+                {
+                    og.join(lnk.as_str()).unwrap()
+                }
+                else
+                {
+                    lnk
+                };
+                if !links.contains(&ur.to_string())
+                {
+                    println!("new link to visit! {}", ur.as_str());
+                    links.push(ur.to_string());
+
+                    Some(tokio::spawn(async move {
+                        get_all_domestic_links(&cl, ur).await
+                    }))
+                }
+                else
+                {
+                    None
+                }
+            })
+            .collect();
+
+        if tasks.is_empty()
+        {
+            links.sort();
+            links.dedup();
+            return Ok(());
+        }
+        for task in tasks
+        {
+            let StdOk(task) = &mut task.await? else {continue};
+            links.append(&mut task.iter().map(|x| x.to_string()).collect_vec());
+        }
+    }
+    let url = links
+        .iter()
+        .filter(|x| !visited.contains(&Url::parse(x).unwrap()))
+        .choose(&mut rand::thread_rng())
+        .and_then(|x| Url::parse(x).ok())
+        .unwrap();
     scrape_all(client, url, visited, links).await
 }
